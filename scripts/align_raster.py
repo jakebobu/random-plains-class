6999a46066b994dff707d0623e0c25b99f63594f
jakebobu <jakebobula@gmail.com>
2021-04-05 02:36:33 -0600

    updates to align raster to clean and add comments

--- scripts/align_raster.py
+++ scripts/align_raster.py
@@ -12,21 +12,48 @@ import numpy as np
 from osgeo import gdal, osr
 
 
-def save_img(data, geotransform, proj, outPath, noDataValue=np.nan, split=False):
-    # Start the gdal driver for GeoTIFF
-    if outPath == "MEM":
-        driver = gdal.GetDriverByName("MEM")
-    else:
-        driver = gdal.GetDriverByName("GTiff")
+def save_data_as_raster(
+    data, geotransform, proj, out_path, no_data_val=np.nan, split=False
+):
+    """
+    saves data array to raster(s)
 
+    there are three paths through this function:
+    1) if the data is 2 dimensions it gets saved as a single band raster
+    2) if the data is 3 dimensions and split is False it gets saved as a multiband raster
+    3) if the data is 3 dimensions and split is True it gets saved as a multipule single band rasters
+
+    Parameters
+    ----------
+    data: array
+        array to be saved to raster
+    geotransform: array
+        determines the data to map affine transform
+    proj: str
+        coordinate system in wkt
+    out_path: str
+        path to save output raster(s) to
+    no_data_val: float
+        value to be used to fill empty
+    split: bool
+        true breaks 3d data into rasters for each 3rd demension slice of the data
+
+    Returns
+    -------
+    paths: list of str
+        file path(s) of output raster(s)
+    """
+
+    driver = gdal.GetDriverByName("GTiff")
     paths = []
     shape = data.shape
     if len(shape) > 2:
         if split:
+            # option 3 explained above
             for i in range(shape[2]):
                 path = os.path.join(
-                    os.path.dirname(outPath),
-                    str(i + 1) + "_" + os.path.basename(outPath),
+                    os.path.dirname(out_path),
+                    str(i + 1) + "_" + os.path.basename(out_path),
                 )
                 ds = driver.Create(
                     path,
@@ -39,13 +66,14 @@ def save_img(data, geotransform, proj, outPath, noDataValue=np.nan, split=False)
                 ds.SetProjection(proj)
                 ds.SetGeoTransform(geotransform)
                 ds.GetRasterBand(1).WriteArray(data[:, :, i])
-                ds.GetRasterBand(1).SetNoDataValue(noDataValue)
+                ds.GetRasterBand(1).SetNoDataValue(no_data_val)
                 ds.FlushCache()
                 ds = None
                 paths.append(path)
         else:
+            # option 2 explained above
             ds = driver.Create(
-                outPath,
+                out_path,
                 shape[1],
                 shape[0],
                 shape[2],
@@ -56,13 +84,14 @@ def save_img(data, geotransform, proj, outPath, noDataValue=np.nan, split=False)
             ds.SetGeoTransform(geotransform)
             for i in range(shape[2]):
                 ds.GetRasterBand(i + 1).WriteArray(data[:, :, i])
-                ds.GetRasterBand(i + 1).SetNoDataValue(noDataValue)
+                ds.GetRasterBand(i + 1).SetNoDataValue(no_data_val)
             ds.FlushCache()
             ds = None
-            paths.append(outPath)
+            paths.append(out_path)
     else:
+        # option 1 explained above
         ds = driver.Create(
-            outPath,
+            out_path,
             shape[1],
             shape[0],
             1,
@@ -72,15 +101,16 @@ def save_img(data, geotransform, proj, outPath, noDataValue=np.nan, split=False)
         ds.SetProjection(proj)
         ds.SetGeoTransform(geotransform)
         ds.GetRasterBand(1).WriteArray(data)
-        ds.GetRasterBand(1).SetNoDataValue(noDataValue)
+        ds.GetRasterBand(1).SetNoDataValue(no_data_val)
         ds.FlushCache()
         ds = None
-        paths.append(outPath)
+        paths.append(out_path)
 
     return paths
 
 
 def get_raster_info(raster_path):
+    # wraps gdal.Info and returns prj, geoinformation, nodata, num_bands
     ret = gdal.Info(raster_path, options="-json")
     num_bands = len(ret["bands"])
     prj = ret["coordinateSystem"]["wkt"]
@@ -94,6 +124,7 @@ def get_raster_info(raster_path):
 
 
 def apply_band_to_filepath(path, band_num):
+    # add band number to filename, returns new path
     band_path = os.path.join(
         os.path.dirname(path), str(band_num) + "_" + os.path.basename(path)
     )
@@ -101,13 +132,15 @@ def apply_band_to_filepath(path, band_num):
 
 
 def get_band_from_filepath(path):
+    # gets band number from filename, returns band number
     num = os.path.basename(path).split("_")[0]
     return num
 
 
 def get_band_raster(input_raster, split_file, current_band):
+    # wraps a gdal translate call to get a specific band of a raster, returns ouput file path
     sleep(randint(1, 20))
-    split_outPath = apply_band_to_filepath(split_file, current_band)
+    split_out_path = apply_band_to_filepath(split_file, current_band)
     subprocess.run(
         [
             "gdal_translate",
@@ -118,25 +151,27 @@ def get_band_raster(input_raster, split_file, current_band):
             "-co",
             "NUM_THREADS=ALL_CPUS",
             input_raster,
-            split_outPath,
+            split_out_path,
         ]
     )
     print("band {} is split".format(current_band))
-    return split_outPath
+    return split_out_path
 
 
 def warp_raster_func(warped_file, split_file_path, kwargs, dest_prj):
+    # wraps gdal.Warp, returns output file path
     sleep(randint(1, 20))
     srs = osr.SpatialReference(wkt=dest_prj)
     band_n = get_band_from_filepath(split_file_path)
-    warp_outPath = apply_band_to_filepath(warped_file, band_n)
+    warp_out_path = apply_band_to_filepath(warped_file, band_n)
     kwargs["dstSRS"] = srs
-    gdal.Warp(warp_outPath, split_file_path, **kwargs)
+    gdal.Warp(warp_out_path, split_file_path, **kwargs)
     print("band {} is warped".format(band_n))
-    return warp_outPath
+    return warp_out_path
 
 
 def align_raster_func(path, alignraster, pickle_filename):
+    # wraps gr.align_rasters, returns ouput filename
     sleep(randint(1, 20))
     band = get_band_from_filepath(path)
     out_filename = apply_band_to_filepath(pickle_filename, band)
@@ -153,17 +188,18 @@ def align_raster_func(path, alignraster, pickle_filename):
 def save_raster_func(
     alignraster_prj, dst_filename, alignraster_nodata, pickle_filename
 ):
+    # wraps save_data_as_raster, returns output file name
     sleep(randint(1, 20))
     with open(pickle_filename, "rb") as f:
         line = pickle.load(f)
     n = get_band_from_filepath(pickle_filename)
     dest_name = apply_band_to_filepath(dst_filename, n)
-    save_img(
+    save_data_as_raster(
         line[0],
         list(line[2]),
         alignraster_prj,
         dest_name,
-        noDataValue=alignraster_nodata,
+        no_data_val=alignraster_nodata,
         split=False,
     )
     print("band {} is saved".format(dest_name))
@@ -171,7 +207,29 @@ def save_raster_func(
 
 
 def align_raster_full(alignraster, input_raster, dst_filename, temp_dir):
+    """
+    align raster to another raster, this is the centeral function of the script
+
+    Parameters
+    ----------
+    dst_filename: str
+        Path to save band rasters
+    input_raster: str
+        Path to input raster
+    alignraster: str
+        path to raster to align to
+    temp_dir: str
+        directory to use for temp files
+
+    Returns
+    -------
+    None
+    """
+
     # NOTE: rasterband is kept track of with integers before an underscore in the file names
+    # see functions apply_band_to_filepath and get_band_from_filepath
+
+    # NOTE: there are random sleep functions liberally sprinkled into the script due previous problems with pool
 
     # this is used for split and warp, it is not used for align or save because of ram limitations
     cpu = multiprocessing.cpu_count() - 1
@@ -183,12 +241,14 @@ def align_raster_full(alignraster, input_raster, dst_filename, temp_dir):
         alignraster_nodata,
         alignraster_num_bands,
     ) = get_raster_info(alignraster)
+
     (
         input_raster_prj,
         input_raster_geoinformation,
         input_raster_nodata,
         input_raster_num_bands,
     ) = get_raster_info(input_raster)
+
     xRes = alignraster_geoinformation[1]
     yRes = alignraster_geoinformation[5]
 
@@ -254,12 +314,12 @@ def align_raster_full(alignraster, input_raster, dst_filename, temp_dir):
 
 
 def get_parser():
-    desc = "make the transforms for the icp json in a directory"
+    desc = "align raster to another raster"
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("--dst_filename", "-d", help="Path to save band rasters")
     parser.add_argument("--raster", "-r", help="Path to input raster")
-    parser.add_argument("--alignraster", "-a", help="path to raster to align to")
-    parser.add_argument("--temp_dir", "-t", help="directory to use for temp files")
+    parser.add_argument("--alignraster", "-a", help="Path to raster to align to")
+    parser.add_argument("--temp_dir", "-t", help="Directory to use for temp files")
     return parser
 
 
@@ -277,6 +337,7 @@ def main(rawargs):
         path to raster to align to
     temp_dir: str
         directory to use for temp files
+
     Returns
     -------
     None
@@ -293,6 +354,7 @@ def main(rawargs):
     args = get_parser().parse_args(rawargs)
 
     dst_filename = args.dst_filename
+    # TODO: add handling for missing argument
 
     raster = args.raster
     if not os.path.exists(raster):
@@ -310,6 +372,8 @@ def main(rawargs):
         sys.exit(1)
 
     align_raster_full(alignraster, raster, dst_filename, temp_dir)
+
+    # TODO: add option to empty temp_dir
 
 
 if __name__ == "__main__":
